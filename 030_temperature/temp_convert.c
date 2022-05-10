b751e828e3ab67159aaadb043fec63b772c1bb65
Pratik <pratikpadman125@gmail.com>
2022-05-09 19:09:34 -0700

    gitinit

--- 030_temperature/temp_convert.c
+++ 030_temperature/temp_convert.c
@@ -15,31 +15,31 @@ int main(int argc, char *argv[]) {
     }
     
     if(argc >= 3)
-    {
-    if(0 == strcmp(argv[2], C[0]) || 0 == strcmp(argv[2], C[1]) || 0 == strcmp(argv[2], C[2]) || 0 == strcmp(argv[2], C[3]) ||0 == strcmp(argv[2], C[4])) {
+    {                                         
+    if( 0 == strcmp(argv[2], C[1]) || 0 == strcmp(argv[2], C[2]) || 0 == strcmp(argv[2], C[3]) ||0 == strcmp(argv[2], C[4]) || (argv[2][1]) == 'C') {
        
         if(0 == strcmp(argv[1], K[0]) || 0 == strcmp(argv[1], K[1]) || 0 == strcmp(argv[1], K[2]) || 0 == strcmp(argv[1], K[3])) {
 
            for(int c = 3; c < argc; c++) {
             temp_c = atof(argv[c]) - 273.5;
-            printf("%f celsius",temp_c);
+            printf("%2f celsius",temp_c);
            }
         }
-        else if(0 == strcmp(argv[1], F[0]) || 0 == strcmp(argv[1], F[1]) || 0 == strcmp(argv[1], F[2]) || 0 == strcmp(argv[1], F[3]) || 0 == strcmp(argv[1], F[4])) {
+        else if( 0 == strcmp(argv[1], F[1]) || 0 == strcmp(argv[1], F[2]) || 0 == strcmp(argv[1], F[3]) || 0 == strcmp(argv[1], F[4]) || (argv[1][1]) == 'F') {
             for(int c = 3; c < argc; c++) {
             temp_c = (atof(argv[c])-32)*(5)/9;
-            printf("%f celsius", temp_c);
+            printf("%2f celsius", temp_c);
             }
         }
     }
-    else if(0 == strcmp(argv[2], F[0]) || 0 == strcmp(argv[2], F[1]) || 0 == strcmp(argv[2], F[2]) || 0 == strcmp(argv[2], F[3]) ||0 == strcmp(argv[2], F[4])){
+    else if( 0 == strcmp(argv[2], F[1]) || 0 == strcmp(argv[2], F[2]) || 0 == strcmp(argv[2], F[3]) ||0 == strcmp(argv[2], F[4]) || (argv[2][1]) == 'F'){
         if(0 == strcmp(argv[1], K[0]) || 0 == strcmp(argv[1], K[1]) || 0 == strcmp(argv[1], K[2]) || 0 == strcmp(argv[1], K[3])) {
            for(int f = 3; f < argc; f++) {
             temp_f = 1.8*(atof(argv[f]) - 273.5)+ 32;
             printf("%f fahrenheit", temp_f);
            }
         }
-        else if(0 == strcmp(argv[1], C[0]) || 0 == strcmp(argv[1], C[1]) || 0 == strcmp(argv[1], C[2]) || 0 == strcmp(argv[1], C[3]) || 0 == strcmp(argv[1], C[4])) {
+        else if( 0 == strcmp(argv[1], C[1]) || 0 == strcmp(argv[1], C[2]) || 0 == strcmp(argv[1], C[3]) || 0 == strcmp(argv[1], C[4]) || (argv[1][1]) == 'C') {
            for(int f = 3; f < argc; f++) {
             temp_f = 1.8*atof(argv[f]) + 32;
             printf("%f fahrenheit", temp_f);
@@ -47,13 +47,13 @@ int main(int argc, char *argv[]) {
         }
     }
     else if(0 == strcmp(argv[2], K[0]) || 0 == strcmp(argv[2], K[1]) || 0 == strcmp(argv[2], K[2]) || 0 == strcmp(argv[2], K[3])) {
-        if(0 == strcmp(argv[1], F[0]) || 0 == strcmp(argv[1], F[1]) || 0 == strcmp(argv[1], F[2]) || 0 == strcmp(argv[1], F[3]) || 0 == strcmp(argv[1], F[4])){
+        if(0 == strcmp(argv[1], F[1]) || 0 == strcmp(argv[1], F[2]) || 0 == strcmp(argv[1], F[3]) || 0 == strcmp(argv[1], F[4]) || (argv[1][1]) == 'F'){
             for(int k = 3; k < argc; k++) {
             temp_k = (0.5555)*(atof(argv[k]) - 32) + 273.5;
             printf("%f kelvin", temp_k);
             }
         }
-        else if(0 == strcmp(argv[1], C[0]) || 0 == strcmp(argv[1], C[1]) || 0 == strcmp(argv[1], C[2]) || 0 == strcmp(argv[1], C[3]) || 0 == strcmp(argv[1], C[4])) {
+        else if( 0 == strcmp(argv[1], C[1]) || 0 == strcmp(argv[1], C[2]) || 0 == strcmp(argv[1], C[3]) || 0 == strcmp(argv[1], C[4]) || (argv[1][1]) == 'C') {
            for(int k = 3; k < argc; k++) {
             temp_k = atof(argv[k]) + 273.5;
             printf("%f kelvin", temp_k);
